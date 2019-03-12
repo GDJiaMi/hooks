422f99aba14e18829c734132f1a939c6929f8957
carney520 <carney520@hotmail.com>
2019-03-12 17:02:32 +0800

    feat: add useInput

---
+++ src/__tests__/useInput.ts
@@ -0,0 +1,23 @@
+import { renderHook, cleanup, act } from "react-hooks-testing-library";
+import { useInput } from "../index";
+
+afterEach(cleanup);
+
+it("should set Value after event triggered", () => {
+  const { result } = renderHook(() => useInput("initial"));
+  expect(result.current.value).toBe("initial");
+
+  act(() => {
+    result.current.onChange({
+      target: { value: "new value" }
+    } as React.ChangeEvent<{ value: string }>);
+  });
+
+  expect(result.current.value).toBe("new value");
+
+  act(() => {
+    result.current.setValue("value by setValue");
+  });
+
+  expect(result.current.value).toBe("value by setValue");
+});
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import useSideEffectState from "./useSideEffectState";
 import useUpdate from "./useUpdate";
 import useSession from "./useSession";
 import usePromise from "./usePromise";
+import useInput from "./useInput";
 
 export {
+  useInput,
   useAsyncOnMount,
   useSideEffectState,
   useUpdate,
