687fb3ee30a4e69e2defe8faef73ec9e36a35aad
carney520 <carney520@hotmail.com>
2019-03-25 22:50:39 +0800

    feat: useInstance 支持使用函数进行初始化

--- src/useInstance.ts
+++ src/useInstance.ts
@@ -1,9 +1,16 @@
-import { useRef, useCallback } from 'react'
+import { useRef, useCallback, MutableRefObject } from 'react'
 
 export default function useInstance<T extends object>(
-  initial: T,
+  initial: T | (() => T),
 ): [T, (value: Partial<T>) => void] {
-  const ref = useRef<T>(initial || {})
+  const initialized = useRef(false)
+  const ref = useRef<T>() as MutableRefObject<T>
+
+  if (!initialized.current) {
+    ref.current =
+      typeof initial === 'function' ? (initial as () => T)() : initial || {}
+    initialized.current = true
+  }
 
   const update = useCallback<(value: Partial<T>) => void>(value => {
     for (let key in value) {
